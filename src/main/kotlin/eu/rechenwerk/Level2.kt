75c582c4b86fcda555cf4b4910e3a2bf4433460d
rechen-werk <adrian.vinojcic@gmail.com>
2024-10-14 15:07:15 +0200

    level2

--- src/main/kotlin/eu/rechenwerk/Level2.kt
+++ src/main/kotlin/eu/rechenwerk/Level2.kt
@@ -1,7 +1,46 @@
 package eu.rechenwerk
 
 import eu.rechenwerk.ccc.external.Level
+import eu.rechenwerk.ccc.external.Many
 
-@Level(2) fun level2(): String {
-    return ""
+@Level(2) fun level2(
+    n: Int,
+    @Many("n", String::class) paths: List<String>
+): String {
+    return paths.map{ path ->
+        val size = path.getPathSize()
+        "${size.first} ${size.second}"
+    }.joinToString("\n")
+}
+
+fun String.getPathSize(): Pair<Int, Int> {
+    var xMax = 0
+    var xMin = 0
+    var yMax = 0
+    var yMin = 0
+
+    var x = 0
+    var y = 0
+
+    for (dir in this) {
+        when(dir) {
+            'W' -> y++
+            'S' -> y--
+            'A' -> x--
+            'D' -> x++
+        }
+        if(x < xMin) {
+            xMin = x
+        }
+        if(x > xMax) {
+            xMax = x
+        }
+        if(y < yMin) {
+            yMin = y
+        }
+        if(y > yMax) {
+            yMax = y
+        }
+    }
+    return Pair(xMax - xMin + 1, yMax - yMin + 1)
 }
